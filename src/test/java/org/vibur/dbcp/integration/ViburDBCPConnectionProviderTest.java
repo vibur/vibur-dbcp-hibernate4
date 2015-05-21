66349ccfac99b343add299ce6c1b41c604631f31
simeonmalchev <simeon.malchev@gmail.com>
2015-05-21 21:36:11 +1000

    test update

--- src/test/java/org/vibur/dbcp/integration/ViburDBCPConnectionProviderTest.java
+++ src/test/java/org/vibur/dbcp/integration/ViburDBCPConnectionProviderTest.java
@@ -108,7 +108,7 @@ public class ViburDBCPConnectionProviderTest {
         assertTrue(mockedStatementCache.containsKey(key1.getValue()));
         assertEquals(key1.getValue(), key2.getValue());
         assertEquals("prepareStatement", key1.getValue().getMethod().getName());
-        assertTrue(val1.getValue().state().get() == AVAILABLE);
+        assertEquals(AVAILABLE, val1.getValue().state().get());
     }
 
     private void executeAndVerifySelectInSession(Session session) {
